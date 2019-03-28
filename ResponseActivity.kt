e6cb4d1a046d3191b0bd1b7dd51a6455b46f232c
likhithareddy12 <32333151+likhithareddy12@users.noreply.github.com>
2019-03-29 00:02:45 +0530

    Update ResponseActivity.kt

--- ResponseActivity.kt
+++ ResponseActivity.kt
@@ -37,22 +37,117 @@ class ResponseActivity : AppCompatActivity() {
         if(ss=="radio46")
         {
             setContentView(R.layout.hotels)
+            if(hotelsr1.isChecked())
+            {
+                intent.putExtra("hotels","r1")
+            }
+            else if(hotelsr2.isChecked())
+            {
+                intent.putExtra("hotels","r2")
+            }
+            else if(hotelsr3.isChecked())
+            {
+                intent.putExtra("hotels","r3")
+            }
+            else if(hotelsr4.isChecked())
+            {
+                intent.putExtra("hotels","r4")
+            }
+            selhotels.setOnClickListener{
+                val intent = Intent(this@ResponseActivity,Map2Activity::class.java)
+                startActivity(intent)
         }
         if(ss=="radio42")
         {
             setContentView(R.layout.parks)
+            if(parksr1.isChecked())
+            {
+                intent.putExtra("parks","r1")
+            }
+            else if(parksr2.isChecked())
+            {
+                intent.putExtra("parks","r2")
+            }
+            else if(parksr3.isChecked())
+            {
+                intent.putExtra("parks","r3")
+            }
+            else if(parksr4.isChecked())
+            {
+                intent.putExtra("parks","r4")
+            }
+            selparks.setOnClickListener{
+                val intent = Intent(this@ResponseActivity,Map2Activity::class.java)
+                startActivity(intent)
         }
         if(ss=="radio43")
         {
             setContentView(R.layout.monuments)
+            if(hmr1.isChecked())
+            {
+                intent.putExtra("hm","r1")
+            }
+            else if(hmr2.isChecked())
+            {
+                intent.putExtra("hm","r2")
+            }
+            else if(hmr3.isChecked())
+            {
+                intent.putExtra("hm","r3")
+            }
+            else if(hmr4.isChecked())
+            {
+                intent.putExtra("hm","r4")
+            }
+            selhm.setOnClickListener{
+                val intent = Intent(this@ResponseActivity,Map2Activity::class.java)
+                startActivity(intent)
         }
         if(ss=="radio44")
         {
             setContentView(R.layout.malls)
+            if(mallr1.isChecked())
+            {
+                intent.putExtra("mall","r1")
+            }
+            else if(mallr2.isChecked())
+            {
+                intent.putExtra("mall","r2")
+            }
+            else if(mallr3.isChecked())
+            {
+                intent.putExtra("mall","r3")
+            }
+            else if(mallr4.isChecked())
+            {
+                intent.putExtra("mall","r4")
+            }
+            selmall.setOnClickListener{
+                val intent = Intent(this@ResponseActivity,Map2Activity::class.java)
+                startActivity(intent)
         }
         if(ss=="radio45")
         {
             setContentView(R.layout.others)
+            if(otherr1.isChecked())
+            {
+                intent.putExtra("other","r1")
+            }
+            else if(otherr2.isChecked())
+            {
+                intent.putExtra("other","r2")
+            }
+            else if(otherr3.isChecked())
+            {
+                intent.putExtra("other","r3")
+            }
+            else if(otherr4.isChecked())
+            {
+                intent.putExtra("other","r4")
+            }
+            selother.setOnClickListener{
+                val intent = Intent(this@ResponseActivity,Map2Activity::class.java)
+                startActivity(intent)
         }
     }
 }
